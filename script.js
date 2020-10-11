5b71be2a889b6f1ae93396c3abe0f28dbf6ee739
tippi-fifestarr <62179036+tippi-fifestarr@users.noreply.github.com>
2020-10-11 17:11:51 -0500

    dandebugged
    hire me button and border fun

---
+++ script.js
@@ -0,0 +1,107 @@
+// use random math to change the quality of red each time
+const colorsArr = ['red', 'blue', 'green', 'orange', 'purple', 'black', 'white']
+const button = document.getElementById("button")
+console.log(button)
+
+// necessary variables for the HUD Popup
+const hudContainer = document.querySelector('.hud-container')
+const hudTimer = document.querySelector('.hud-timer-box')
+const hudRestTimer = document.querySelector('.hud-rester')
+const closeButt = document.querySelector('.closeButt')
+// timer
+let timer = 0;
+setTimer()
+let x;
+
+// function for colorchange
+window.onclick = myFunction;
+// https://www.w3schools.com/jsref/event_onclick.asp
+// If the user clicks in the window, set the background color of <body> to yellow
+function myFunction() {
+    const randomNum = Math.floor(Math.random() * Math.floor(colorsArr.length))
+    document.getElementsByTagName("BODY")[0].style.background = colorsArr[randomNum]
+    button.style.border = `1px solid ${colorsArr[randomNum]}`
+    if (colorsArr[randomNum] == 'red'){
+      button.onclick = () => {
+        //   open a modal with daduece.html in it
+        togOverlay();
+        winText();
+        // or open a you win screen (timer, and link to dadeuce)
+      }
+    }
+}
+
+
+// this is a mess or is it? it makes sense!
+function setTimer(){
+    // timer += 3??? 
+    // hudTimer.innerHTML = `${timer} sec`
+    // callback function
+    let x = setInterval(function() {
+        // closeButt.onclick = function(){
+        //     hudContainer.classList.remove('show')
+        //     clearInterval(x)
+        // }
+        // hudTimer.innerHTML = `${timer} sec`
+      
+            //   clearInterval(x)  
+            
+          timer++
+    }, 999)
+}
+
+function togOverlay(){
+  console.log('YEP')
+  // the essence of setOverlay without the ifs
+  hudContainer.classList.add('show')
+}
+
+function winText(x) {
+    closeButt.onclick = function(){
+        hudContainer.classList.remove('show')
+        clearInterval(x)
+    }
+    if (timer <= 68){
+        clearInterval(x)
+        hudTimer.innerHTML = `you won in ${timer} seconds, very nice`
+
+    }
+    if (timer >= 69){
+        hudTimer.innerHTML = `you won in more than 69 seconds, try again!` 
+        clearInterval(x) 
+    }s
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // copyFunc copies the text to your clipboard
+// function copyFunc() {
+//   /* Get the text field */
+//   var copyText = document.getElementById("clipboard");
+
+//   /* Select the text field */
+//   copyText.select();
+//   copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+
+//   /* Copy the text inside the text field */
+//   document.execCommand("copy");
+
+//   /* Alert the copied text */
+//   alert("Copied the text: " + copyText.value);
+// }
